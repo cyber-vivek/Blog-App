75288b8411b6dd8c4e6b61e223284aa8771370a4
vivek <vivekkr3580@gmail.com>
2022-07-12 01:13:35 +0530

    testing deploy

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from flask import request
 from .models import Post
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from django.contrib.auth.models import User
--- custom_storage/custom_azure.py
+++ custom_storage/custom_azure.py
@@ -2,7 +2,7 @@ from storages.backends.azure_storage import AzureStorage
 import os;
 
 class AzureMediaStorage(AzureStorage):
-    account_name = os.environ.get('AZURE_ACCOUT_NAME',)  # Must be replaced by your <storage_account_name>
+    account_name = os.environ.get('AZURE_ACCOUT_NAME','djangofiles')  # Must be replaced by your <storage_account_name>
     account_key = os.environ.get('AZURE_ACCOUNT_PASS') # Must be replaced by your <storage_account_key>
     azure_container = 'django-blog-files'
     expiration_secs = None
